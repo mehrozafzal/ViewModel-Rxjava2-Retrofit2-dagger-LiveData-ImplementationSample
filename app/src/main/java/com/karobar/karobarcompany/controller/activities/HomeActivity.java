435611d8a7b569ce9b637c6c334b8ded3b9094e5
Mezzy <mehroz.mehar99@gmail.com>
2018-09-28 01:42:21 +0500

    Navigation drawer added

--- app/src/main/java/com/karobar/karobarcompany/controller/activities/HomeActivity.java
+++ app/src/main/java/com/karobar/karobarcompany/controller/activities/HomeActivity.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.karobar.karobarcompany.R;
+
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,6 +33,11 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     DrawerLayout drawerLayout;
     @BindView(R.id.drawer_content)
     View drawerContent;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    private TextView toolBarTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +49,32 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     private void bindViews() {
         dashBoardContainer = drawerContent.findViewById(R.id.dashBoardContainer);
+        toolBarTitle = drawerContent.findViewById(R.id.toolbar_title);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            public void onDrawerClosed(View view) {
+                supportInvalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                supportInvalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, 0);
+                drawerContent.setTranslationX(slideOffset * drawerView.getWidth());
+                drawerLayout.bringChildToFront(drawerView);
+                drawerLayout.requestLayout();
+            }
+        };
+
+        drawerLayout.addDrawerListener(toggle);
+        drawerLayout.setScrimColor(getResources().getColor(android.R.color.transparent));
+        toggle.syncState();
+        navView.setNavigationItemSelectedListener(this);
+    //    navView.setCheckedItem(R.id.nav_menu_home);
     }
 
     @Override
--- app/src/main/java/com/karobar/karobarcompany/controller/activities/IntroductionActivity.java
+++ app/src/main/java/com/karobar/karobarcompany/controller/activities/IntroductionActivity.java
@@ -5,8 +5,8 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.Window;
-import android.view.WindowManager;
+import android.support.v4.view.ViewPager;
+import android.view.View;
 
 import com.github.paolorotolo.appintro.AppIntro;
 import com.github.paolorotolo.appintro.AppIntroFragment;
@@ -20,13 +20,12 @@ public class IntroductionActivity extends AppIntro {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setFadeAnimation();
         addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.title1), getResources().getString(R.string.description1), R.drawable.ic_karobar_smartphone, getResources().getColor(R.color.intro_color1)));
         addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.title2), getResources().getString(R.string.description2), R.drawable.ic_karobar_manage, getResources().getColor(R.color.intro_color2)));
         addSlide(AppIntroFragment.newInstance(getResources().getString(R.string.title3), getResources().getString(R.string.description3), R.drawable.ic_karobar_products, getResources().getColor(R.color.intro_color3)));
         // OPTIONAL METHODS.
+        setCustomTransformer(new ZoomOutPageTransformer());
         setBarColor(Color.parseColor("#90000000"));
         setSeparatorColor(Color.parseColor("#7351B2"));
         showSkipButton(true);
@@ -60,5 +59,44 @@ public class IntroductionActivity extends AppIntro {
     public void onBackPressed() {
         finish();
         System.exit(0);
+    }
+
+    public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+            int pageHeight = view.getHeight();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+
+            } else if (position <= 1) { // [-1,1]
+                // Modify the default slide transition to shrink the page as well
+                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0) {
+                    view.setTranslationX(horzMargin - vertMargin / 2);
+                } else {
+                    view.setTranslationX(-horzMargin + vertMargin / 2);
+                }
+
+                // Scale the page down (between MIN_SCALE and 1)
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+                // Fade the page relative to its size.
+                view.setAlpha(MIN_ALPHA +
+                        (scaleFactor - MIN_SCALE) /
+                                (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0);
+            }
+        }
     }
 }
